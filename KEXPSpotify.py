9dac2b2c4ee1c4e274bf9375aa3d73384a14b304
rbsysad@gmail.com <rbsysad@gmail.com>
2023-01-28 13:43:12 -0600

    Simplify program with spotipy and no local file storage

--- KEXPSpotify.py
+++ KEXPSpotify.py
@@ -1,52 +1,74 @@
 import requests
-import json
-from shadow import spotify_user_id, playlist_id
-from refresh import Refresh
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import cred
+
 
 class KEXPSpotify:
 
     def __init__(self):
-        self.user_id = spotify_user_id
-        self.playlist_id = playlist_id
-        self.token = ""
+        self.user_id = cred.spotify_user_id
+        self.playlist_id = cred.playlist_id
         self.artist = ""
         self.track = ""
         self.track_list = []
         self.kexp_endpoint = "https://api.kexp.org/v1/play"
+        self.spotify = ""
+        self.uri_list = []
 
-    def call_refresh(self):
-    # REFRESH API ACCESS TOKEN
-        print("---REFRESHING TOKEN---")
-        refreshCaller = Refresh()
-        self.token = refreshCaller.refresh()
+    def spotify_authorization(self):
+        # AUTHORIZE USING SPOTIPY
 
-    def get_songs(self):
-    # GET LIST OF SONGS FROM KEXP    
-        print("---GETTING SONGS---")
+        print("---AUTHORIZING---")
+
+        self.spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(
+            client_id=cred.client_ID,
+            client_secret=cred.client_SECRET,
+            redirect_uri=cred.redirect_url,
+            scope=cred.scope))
+
+        print(self.spotify)
+
+    def get_kexp_songs(self):
+        # GET LIST OF SONGS FROM KEXP
+
+        print("---GETTING KEXP SONGS---")
         response = requests.get('https://api.kexp.org/v1/play/')
         response_json = (response.json())
-        with open("file.txt") as file:
-            for item in response_json['results']:
-                # if isinstance(item['artist'], dict):
-                #   if isinstance(item['track'], dict):
-                #     self.artist = item['artist']['name']
-                #     self.track = item['track']['name']
-                # self.track_list.append(f"{self.artist} {self.track}")
-                file.write()
-        file.close()
+        for item in response_json['results']:
+            if isinstance(item['artist'], dict):
+                if isinstance(item['track'], dict):
+                    self.artist = item['artist']['name']
+                    self.track = item['track']['name']
+
+                    formatted_artist = self.artist
+                    formatted_track = self.track
+                    formatted_string = "track:{} artist:{}".format(formatted_track, formatted_artist)
+                    self.track_list.append(formatted_string)
 
         print(self.track_list)
-        
-    def search_for_song(self):
-    # ADD SONGS TO SPOTIFY PLAYLIST
-    
-            for song in self.track_list:
-                print(f"---SEARCHING FOR {song}---")
-                print()
-                query = f"https://api.spotify.com/v1/search?q={song}&limit=1&type=track&market=US"
-                response = requests.get(query, headers={"Content-Type": "application/json",
-                                                    "Authorization": f"Bearer {self.token}"})
 
-                response_json = response.json()
+    def get_wtmd_songs(self):
+        # GET LIST OF SONGS FROM WTMD
 
-                
+        print("---GETTING WTMD SONGS---")
+        response = requests.get
+
+    def search_spotify(self):
+        # SEARCH SPOTIFY FOR SONGS
+        # RETURNS SPOTIFY URI
+
+        for song in self.track_list:
+            print(f"---SEARCHING FOR {song}---")
+            print()
+
+            q = str(song)
+            results = self.spotify.search(q, limit=1, offset=0, market="US")
+
+            for item in results["tracks"]["items"]:
+                self.uri_list.append(item["uri"])
+
+    def add_to_playlist(self):
+        print("---ADDING SONGS TO PLAYLIST---")
+        self.spotify.playlist_add_items(self.playlist_id, self.uri_list, position=None)
+
--- main.py
+++
@@ -1,6 +0,0 @@
-from KEXPSpotify import KEXPSpotify
-
-a = KEXPSpotify()
-a.get_songs()
-# a.call_refresh()
-# a.search_for_song()
